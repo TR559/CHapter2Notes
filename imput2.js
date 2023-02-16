763c1bf8e347a6e1e897017a235c77845bfa5446
TR559 <uzy5@fresnou.org>
2023-02-16 15:58:37 -0800

    initial commit

---
+++ imput2.js
@@ -0,0 +1,37 @@
+const readline = require ('readline-sync');
+// let number1 = Number(readline.question("Enter a number"));
+// let number2 = Number(readline.question("Enter a number"));
+// console.log(`${number1} + ${number2} = ${number1 +  number2}`)
+
+// let number3 = Number(readline.question("Enter a number"));
+// let number4 = Number(readline.question("Enter a number"));
+// console.log(`${number3} - ${number4} = ${number3 -  number4}`)
+
+// let number5 = Number(readline.question("Enter a number"));
+// let number6 = Number(readline.question("Enter a number"));
+// console.log(`${number5} * ${number6} = ${number5 *  number6}`)
+
+// let number7 = Number(readline.question("Enter a number"));
+// let number8 = Number(readline.question("Enter a number"));
+// console.log(`${number7} / ${number8} = ${number7 / number8}`)
+
+// let number9 = Number(readline.question("Enter a number"));
+// let number10 = Number(readline.question("Enter a number"));
+// console.log(`${number9} % ${number10} = ${number9 %  number10}`)
+
+// let userName1 = readline.question("Enter your first name:");
+// let userName2 = readline.question("Enter your last name:");
+// console.log(`Hello ${userName2}, ${userName1}`)
+
+// let userName3 = readline.question("Enter your last name :");
+// let userName4 = readline.question("Enter your title :");
+// let userName5 = readline.question("Enter your Suffix :");
+// console.log(`Hello ${userName4}.${userName3}, ${userName5}`)
+
+// let userName11 = readline.question("Enter your house number:");
+// let userName6 = readline.question("Enter your street name:");
+// let userName7 = readline.question("Enter your Street type:");
+// let userName8 = readline.question("Enter your zip code:");
+// let userName9 = readline.question("Enter your city:");
+// let userName10 = readline.question("Enter your state:");
+// console.log (`${userName11} ${userName6} ${userName7} ${userName8} ${userName9} ${userName10}`)
