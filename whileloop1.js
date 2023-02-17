9a83fcd832ec684186673fd870e4cdb0a540a344
TR559 <uzy5@fresnou.org>
2023-02-17 14:16:13 -0800

    learned whileloops

---
+++ whileloop1.js
@@ -0,0 +1,53 @@
+// let x = 0;
+//
+// while (x < 26){
+//
+// console.log(x);
+//
+// x++;
+//
+// }
+
+// let x = 0;
+//
+// while (x < 51){
+//
+// console.log(x);
+//
+// x++;
+//
+// }
+
+
+// let x = -10;
+//
+// while (x < 11){
+//
+//     console.log(x);
+//
+//     x++;
+//
+// }
+
+
+// let x = -51;
+//
+// while (x < 1){
+//
+//     console.log(x);
+//
+//     x++;
+//
+// }
+
+
+// let x = 0;
+//
+// while (x < 101){
+//     if (x )
+//
+//     console.log(x);
+//
+//     x++;
+//
+// }
